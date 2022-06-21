a0cfd45344a8f884be3e2a1a4a2308e4b967aec2
Debopriyo <chatterjeedebopriyo05@gmail.com>
2022-06-21 17:34:18 +0530

    for a single demo

---
+++ myjava_project/src/module-info.java
@@ -0,0 +1,2 @@
+module myjava_project {
+}
---
+++ myjava_project/src/myjava_project/Democlass.java
@@ -0,0 +1,9 @@
+package myjava_project;
+
+public class Democlass {
+
+	public static void main(String[] args) {
+		
+	System.out.println("Halow world");
+	}
+}
